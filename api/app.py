97122325b028769e09659606bd3461ca16e2327e
Mahosaurus <metaphigmamentality@web.de>
2023-02-14 15:53:29 +0000

    small test

--- api/app.py
+++ api/app.py
@@ -7,4 +7,4 @@ app = init_app()
 app.config.from_pyfile(path.abspath(path.dirname(__file__)) + "/flask_config.py")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=False)
+    app.run(host="0.0.0.0", port=8080, debug=True)
