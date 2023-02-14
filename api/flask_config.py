e22dfef5eabf1d478574c3a77072f5359375f2b5
Mahosaurus <metaphigmamentality@web.de>
2023-02-14 16:19:15 +0000

    first shot

--- api/flask_config.py
+++ api/flask_config.py
@@ -8,9 +8,11 @@ if os.environ.get("DEPLOY_ENV") == "production":
     DEBUG = False
     Testing = False
     DATA_PATH = os.path.join("/home", "site", "cache.json")
+    UPLOAD_FOLDER = "/tmp"
 else:
     ENV = 'development'
     FLASK_ENV = 'development'
     DEBUG = True
     Testing = True
     DATA_PATH = os.path.join(get_repo_root(), "src", "example_data", "cache.json")
+    UPLOAD_FOLDER = "/tmp"
