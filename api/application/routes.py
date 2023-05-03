aad83b6fb08482089fa01775e5ab46c31e2b391c
Mahosaurus <metaphigmamentality@web.de>
2023-05-03 08:26:28 +0000

    some improvements

--- api/application/routes.py
+++ api/application/routes.py
@@ -1,8 +1,10 @@
 import os
 
+import cv2
+import pytesseract
+
 from flask import current_app as app
-from flask import request, redirect, flash, url_for, render_template
-from werkzeug.utils import secure_filename
+from flask import request, redirect, flash, render_template
 
 def allowed_file(filename):
     ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
@@ -16,15 +18,31 @@ def upload_file():
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
+
         file = request.files['file']
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
+
         if file and allowed_file(file.filename):
-            #TODO: Enter the tesseract code.
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('upload_file', name=filename))
-    return render_template("template.html")
+            print("Tesseract start")
+            file.save("tmp")
+            image = cv2.imread("tmp") #pylint: disable=no-member
+            result = ocr_by_tesseract(image)
+            print(result)
+            return render_template("template.html", result={"text": result})
+    else:
+        return render_template("template.html", result={})
+
+def ocr_by_tesseract(file_contet):
+    # Get an indexable document
+    result_text = bytes(pytesseract.image_to_string(file_contet, lang='deu'), 'utf-8').decode('utf-8')
+    result_text = result_text.replace("\n", "")
+    result_text = result_text.replace(",", " ")
+    result_text = result_text.replace("-", " ")
+    result_text = result_text.replace("—", " ")
+    result_text = result_text.replace("—", " ")
+    result_text = " ".join([text.lower() for text in result_text.split(" ") if text])
+    return result_text
